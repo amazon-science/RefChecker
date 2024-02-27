65f113f6d4a5dc3a809761744a528dcd33ed0d47
zthang <648793486@qq.com>
2024-02-27 15:49:42 +0800

    Support batching inference.

--- refchecker/checker/__init__.py
+++ refchecker/checker/__init__.py
@@ -1,5 +1,3 @@
-from .claude2_checker import Claude2Checker
-from .gpt4_checker import GPT4Checker
 from .nli_checker import NLIChecker
 from .alignscore.alignscore_checker import AlignScoreChecker
 from .repc.repc_checker import RepCChecker
